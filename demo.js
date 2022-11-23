74f9a07e41c720e1a894ed1a8170bf4da5988f41
Svetlana <124veg@gmail.com>
2022-11-23 14:59:12 +1100

    first commit

--- demo.js
+++ demo.js
@@ -1,12 +1,18 @@
 let productNameInputField = document.getElementById('product-name');
 let theRemainingChars = document.getElementById('remaining-chars');
 
+let maxAllowedChars = productNameInputField.maxLength;
+
 function updateRemainingCharacters(event) {
 let enteredText = event.target.value;
 let enteredTextLength = enteredText.length;
 
-let remainingCharacters = 60 - enteredTextLength;
+let remainingCharacters = maxAllowedChars - enteredTextLength;
+
+theRemainingChars.textContent = remainingCharacters;
 }
+
+
 
 productNameInputField.addEventListener('input', updateRemainingCharacters);
 
