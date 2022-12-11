3cbe30e2ccdabaa472a1a0f9ab655f49d0237b3e
Svetlana <124veg@gmail.com>
2022-12-11 18:56:54 +1100

    added loops.js

--- conditional-code.js
+++ conditional-code.js
@@ -3,3 +3,11 @@ const myName = 'Lana';
 if (myName === 'max') {
 console.log("Hello")
 }
+
+let isLogged = true;
+
+const enteredUserName = "Lana";
+
+if (enteredUserName.length > 0) {
+console.log('Valid')
+}
