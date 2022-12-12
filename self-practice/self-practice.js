bdb7e0eae5872e6697cc0f3e445c0597e71468fb
Svetlana <124veg@gmail.com>
2022-12-12 17:45:19 +1100

    self practice

---
+++ self-practice/self-practice.js
@@ -0,0 +1,18 @@
+const mainButton = document.getElementById('submit-button');
+const userNumberField = document.querySelector("input[type=number]");
+const outputResult = document.getElementById("output-sum");
+
+function sumUpToNumber() {
+   const inputValue = userNumberField.value
+   
+let summedUpNumbers = 0;
+
+for (let i = 0; i <= inputValue; i++) {
+summedUpNumbers = summedUpNumbers + i;
+}
+
+outputResult.textContent = summedUpNumbers;
+
+};
+
+mainButton.addEventListener('click', sumUpToNumber);
